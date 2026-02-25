9de480185bdd93932dc60994b52472e7b1682fb9
JubiloDenise02 <jubilodeniseira@gmail.com>
2026-02-25 22:20:21 +0800

    Created Header Section

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +27,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased bg-[#372c2e] text-[#ffffff] `}
       >
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
